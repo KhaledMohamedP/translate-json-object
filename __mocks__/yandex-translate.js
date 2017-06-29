7e0ea9d52de5c5d5cf588d498af1f9d8acbbc5ef
Khaled Mohamed <khaledmohamedp@gmail.com>
2017-06-29 00:41:21 -0400

    fix(google-translate): Google failed when we translate empty string array, skip empty string or arra

--- __mocks__/yandex-translate.js
+++ __mocks__/yandex-translate.js
@@ -3,7 +3,7 @@ module.exports = function () {
 		translate: (value, lang, fn) => {
 			if (value) {
 				var translatedList = value + '-' + lang.to;
-				fn(null, {text: [translatedList]});
+				fn(null, {code: 200, text: [translatedList]});
 			} else {
 				fn({error: 'provide value'});
 			}

199c19f7e1f01bcd82883fd79b6372fa16acb05a
Khaled Mohamed <khaledmohamedp@gmail.com>
2017-06-29 21:39:31 -0400

    fix(tjo): require language param and fix mocked data

--- __mocks__/mock-data.js
+++ __mocks__/mock-data.js
@@ -1,6 +1,6 @@
 
-const mockDataObject = {name: 'name', some: [{name: 'j'}], addressField: ['city', 'state', 'zipdecode'], bool: true, num: 33, badBool: false, nully: null, undefined: undefined};
-const transaltedES = {name: 'name-es', some: [{name: 'j-es'}], addressField: ['city-es', 'state-es', 'zipdecode-es'], bool: true, num: 33, badBool: false, nully: null, undfined: undefined};
+const mockDataObject = {name: 'name', some: [{name: 'j'}], addressField: ['city', 'state', 'zipdecode'], bool: true, num: 33, badBool: false, nully: null, undefin: undefined};
+const transaltedES = {name: 'name-es', some: [{name: 'j-es'}], addressField: ['city-es', 'state-es', 'zipdecode-es'], bool: true, num: 33, badBool: false, nully: null, undefin: undefined};
 
 module.exports = {
 	mockDataObject,

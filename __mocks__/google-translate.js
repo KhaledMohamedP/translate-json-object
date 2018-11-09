8bc588adcd31c0d7bf511b97366fe5c6173bc641
Simon Dragsbæk Petersen <simon@introdus.dk>
2018-11-09 11:06:24 +0100

    fix(linting): Fixed linting across the whole module

--- __mocks__/google-translate.js
+++ __mocks__/google-translate.js
@@ -2,7 +2,7 @@ module.exports = function () {
 	return {
 		translate: (value, lang, fn) => {
 			if (value && lang) {
-				var translatedList = {translatedText: value + '-' + lang};
+				const translatedList = {translatedText: value + '-' + lang};
 				fn(null, translatedList);
 			} else if (lang === null) {
 				fn('You missed to pass the languge');
--- __mocks__/yandex-translate.js
+++ __mocks__/yandex-translate.js
@@ -2,7 +2,7 @@ module.exports = function () {
 	return {
 		translate: (value, lang, fn) => {
 			if (value && lang.to) {
-				var translatedList = value + '-' + lang.to;
+				const translatedList = value + '-' + lang.to;
 				fn(null, {code: 200, text: [translatedList]});
 			} else if (lang.to === null) {
 				fn(null, {status: 404, error: 'Invalid language'});

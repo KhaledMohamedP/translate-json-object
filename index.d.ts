d2f0a12c9bf651c82055e0472d4e6a047f7932d4
Robert Sasak <robert.sasak@gmail.com>
2020-09-20 10:58:25 +0200

    Typescript support

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare module 'translate-json-object' {
+    interface Options {
+        googleApiKey?: string
+        yandexApiKey?: string
+    }
+
+    function init(options: Options): boolean
+    function translate(srcObj: object, language: any): Promise<object>
+
+    interface Result {
+        init: typeof init
+        translate: typeof translate
+    }
+
+    function TranslateJSONObject(): Result
+
+    export default TranslateJSONObject
+}
